83f96da4b078d3c6ee3afda33e6a3210b9428258
mazed <mazedur@gmail.com>
2020-10-27 12:39:31 +0600

    Vectors in R with rep() functions

---
+++ Day02/Session05.R
@@ -0,0 +1,24 @@
+#Vector in R with rep() function
+x <- rep(5,10)
+x
+
+y<- rep('Mazed',10)
+y
+
+z<- rep(c(1,2,4),5)
+z
+
+w<- rep(c(1,2,4),each=5)
+w
+
+p<-rep(2:8,each=5)
+p
+
+q<- numeric(4)
+q
+
+k<- character(3)
+k
+
+r<- logical(4)
+r
