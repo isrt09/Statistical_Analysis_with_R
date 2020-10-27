c2e225818a1b84255b951d33dbea2e72ebd2cb97
mazed <mazedur@gmail.com>
2020-10-27 11:06:43 +0600

    vectors in R using c() function

---
+++ Day02/Session03.R
@@ -0,0 +1,30 @@
+# Vectors in R using c() functions
+
+ x <- c(10,20,30,40)
+ x
+
+class(x) 
+typeof(x)
+
+y<-c("a","b","c") 
+y
+
+class(y) 
+typeof(y)
+
+z<- c(1,2,3,TRUE, FALSE,"A","b")
+z
+
+class(z) 
+typeof(z)
+
+length(z)
+
+z <-c(x,100)
+z
+
+q<- c(c(1,2,3),25)
+q
+
+s  <- c(c(1,2,3),c(4,5,6))
+s
