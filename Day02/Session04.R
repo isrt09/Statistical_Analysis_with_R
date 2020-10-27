312f0708ebff6d85326baf9b0045b6eaa8a45339
mazed <mazedur@gmail.com>
2020-10-27 11:14:56 +0600

    vectors in R using colon(:) function

---
+++ Day02/Session04.R
@@ -0,0 +1,30 @@
+#Vector in R using colon (:) operator
+
+x <- 1:5
+x
+
+class(x)
+typeof(x)
+
+y<- c(1,2,3,4,5)
+y
+ 
+class(y)
+typeof(y)
+
+z<- 10:1
+z
+
+i<- 3
+j<- 9
+k<- i:j
+k
+
+class(k)
+typeof(k)
+
+p<- c(1:10,20:-3)
+p
+
+w<- 10:-5
+w
